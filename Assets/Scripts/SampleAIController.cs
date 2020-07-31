e1bd0aa62b9193a2c0bf95bc47f452d0d82f6ea1
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-07-30 23:38:47 -0700

    Waypoints Update 2
    Finished the AI Waypoint Loop states.

--- Assets/Scripts/SampleAIController.cs
+++ Assets/Scripts/SampleAIController.cs
@@ -11,7 +11,15 @@ public class SampleAIController : Controller
     //data
     private int currentWaypoint = 0; //for ai's current valyue in waypoint index
     public float closeEnough = 1.0f; //value forif our ai is close enough to its waypoint
+    public LoopType loopType; //for AI patrol loop type
+    private bool isPatrolForward = true; //bool for wether or not we are patrolling foward
 
+    public enum LoopType 
+    {
+        Stop,
+        Loop,
+        PingPong
+    }
     void Awake()
     {
         tf = GetComponent<Transform>();
@@ -25,17 +33,52 @@ public class SampleAIController : Controller
     // Update is called once per frame
     void Update()
     {
+        switch (loopType) 
+        {
+            case LoopType.Stop:
+                Stop();
+                break;
+            case LoopType.Loop:
+                Loop();
+                break;
+            case LoopType.PingPong:
+                PingPong();
+                break;
+        }
+    }
+
+    public void Stop()
+    {
         if (motor.RotateTowards(waypoints[currentWaypoint].position, pawn.turnSpeed))
         {
             //Do nothing!
         }
-        else 
+        else
         {
             //Move forward
             motor.Move(pawn.moveSpeed);
         }
         //if close to waypoint
-        if (Vector3.SqrMagnitude (waypoints[currentWaypoint].position - tf.position) < closeEnough) 
+        if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < closeEnough)
+        {
+            //move to the next patrol waypoint
+            currentWaypoint++;
+        }
+    }
+
+    public void Loop() 
+    {
+        if (motor.RotateTowards(waypoints[currentWaypoint].position, pawn.turnSpeed))
+        {
+            //Do nothing!
+        }
+        else
+        {
+            //Move forward
+            motor.Move(pawn.moveSpeed);
+        }
+        //if close to waypoint
+        if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < closeEnough)
         {
             //and if the waypoint index hasn't been completed
             if (currentWaypoint < waypoints.Count - 1)
@@ -51,4 +94,66 @@ public class SampleAIController : Controller
             }
         }
     }
+
+    public void PingPong()
+    {
+        if (isPatrolForward)
+        {
+            if (motor.RotateTowards(waypoints[currentWaypoint].position, pawn.turnSpeed))
+            {
+                //Do nothing!
+            }
+            else
+            {
+                //Move forward
+                motor.Move(pawn.moveSpeed);
+            }
+            //if close to waypoint
+            if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < closeEnough)
+            {
+                //and if the waypoint index hasn't been completed
+                if (currentWaypoint < waypoints.Count - 1)
+                {
+                    //move to the next patrol waypoint
+                    currentWaypoint++;
+                }
+                else
+                {
+                    //Otherwise reverse direction and decrement our current waypoint
+                    isPatrolForward = false;
+                    currentWaypoint--;
+                }
+            }
+        }
+
+        else
+        {
+            if (motor.RotateTowards(waypoints[currentWaypoint].position, pawn.turnSpeed))
+            {
+                //Do nothing!
+            }
+            else
+            {
+                //Move forward
+                motor.Move(pawn.moveSpeed);
+            }
+            //if close to waypoint
+            if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < closeEnough)
+            {
+                //and if the waypoint index hasn't been completed
+                if (currentWaypoint > 0)
+                {
+                    //move to the next patrol waypoint
+                    currentWaypoint--;
+                }
+                else
+                {
+                    //Otherwise reverse direction and increment our current waypoint
+                    isPatrolForward = true;
+                    currentWaypoint++;
+                }
+            }
+        }
+    }
+
 }
