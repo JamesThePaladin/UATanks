e93053b7660691603d975d3d490fd65c48893778
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-08-06 22:12:43 -0700

    PowerUp Update
    Added PowerUp Class to the game and tested it with a script called CheatController. Will Leave CheatController in the SampleScript folder. Also, created a scene for testing single things.

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PowerUp
+{
+    //modifiers
+    public float speedModifier;
+    public float healthModifier;
+    public float maxHealthModifier;
+    public float fireRateModifier;
+    public bool isPermanent;
+    //powerup duration
+    public float duration;
+    public float durationReset;
+
+    //increase stats by stat modifier
+    //fire rate is subtracted because we are decreasing the delay between each shot
+    public void OnActivate(Pawn target) 
+    {
+        target.moveSpeed *= speedModifier;
+        target.health *= healthModifier;
+        target.maxHealth *= maxHealthModifier;
+        target.shotTimerDelay -= fireRateModifier;
+    }
+
+    //decrease stats to original values
+    public void OnDeactivate(Pawn target) 
+    {
+        target.moveSpeed /= speedModifier;
+        target.health /= healthModifier;
+        target.maxHealth /= maxHealthModifier;
+        target.shotTimerDelay += fireRateModifier;
+    }
+}
---
+++ Assets/Scripts/SampleScripts/CheatController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatController : MonoBehaviour
+{
+    public PowerUpManager powMan;
+    public PowerUp cheatPow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (powMan == null) 
+        {
+            powMan = gameObject.GetComponent<PowerUpManager>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J)) 
+        {
+            //Add powerup to the tank
+            powMan.Add(cheatPow);
+        }
+    }
+}
